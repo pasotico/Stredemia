36d4a0a687f418902383c86d7ae7c67841cf6401
pasotico <pasotolol@gmail.com>
2026-04-13 16:10:46 -0500

    Leer xlsx con 322 registros, filtrar cuantos estudiantes  entre SÍ y NO resportan estrés, imprimir

--- .venv/Codes/phase_3.py
+++ .venv/Codes/phase_3.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
+xlsx =  pd.read_excel('../datos/raw/TEST_ ESTRÉS ACADÉMICO(1-322).xlsx')
+sí_filter = next(c for c in xlsx.columns if 'Durante el transcurso ' in c)
 
+nm = xlsx[xlsx[sí_filter] == 'SI'].reset_index(drop=True)
+nmn = xlsx[xlsx[sí_filter] == 'NO'].reset_index(drop=True)
+
+print(f"\nEstudiantes que reportaron estrés (SI): {len(nm)}")
+print(f"\nEstudiantes que no reportaron estrés (NO): {len(nmn)}")
