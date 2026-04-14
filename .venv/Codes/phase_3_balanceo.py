14f82c6923dcf0903d2e4725a83cbf531feb5635
pasotico <pasotolol@gmail.com>
2026-04-14 00:41:46 -0500

    Correción. Train 0.30 valuated 0.15 test 0.15

--- .venv/Codes/phase_3_balanceo.py
+++ .venv/Codes/phase_3_balanceo.py
@@ -17,6 +17,16 @@ for col in cols_texto:
 X = csv.drop(columns=['nivel_estres', 'puntaje_sisco'])
 y = csv['nivel_estres']
 
-X_train, X_test, y_train, y_test = seg(
-    X, y, test_size=0.30, random_state=42, stratify=y
+X_train, X_temp, y_train, y_temp = seg(
+    X, y,
+    test_size=0.30,
+    random_state=42,
+    stratify=y
+)
+
+X_val, X_test, y_val, y_test = seg(
+    X_temp, y_temp,
+    test_size=0.50,  # mitad de 30% = 15%
+    random_state=42,
+    stratify=y_temp
 )
