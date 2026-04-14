33e5df02d6cb1e07ddd424c8546599521fa2ed30
pasotico <pasotolol@gmail.com>
2026-04-14 00:31:22 -0500

    Codificación de columnas categoricas a numeros

--- .venv/Codes/phase_3_balanceo.py
+++ .venv/Codes/phase_3_balanceo.py
@@ -2,7 +2,14 @@ import pandas as pd
 from collections import Counter as cont
 from imblearn.over_sampling import SMOTE as smote
 from sklearn.model_selection import train_test_split as seg
-from sklearn.preprocessing import LabelEncoder as co
+from sklearn.preprocessing import LabelEncoder
 
 csv = pd.read_csv("../datos/Processed/producto_fase3.csv")
 
+cols_texto = df.select_dtypes(include=['object', 'string']).columns.tolist()
+cols_texto = [c for c in cols_texto if c != 'nivel_estres']
+encoders = {}
+for col in cols_texto:
+    le = LabelEncoder()
+    csv[col] = le.fit_transform(csv[col].astype(str))
+    encoders[col] = le
