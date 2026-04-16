ea2f15be7535ac116a782f33475039396c6f2189
pasotico <pasotolol@gmail.com>
2026-04-15 19:21:23 -0500

    Capa desing, creacion de mapeo para SISCO y conversiones

---
+++ .venv/Interfaz/desing/Mappings.py
@@ -0,0 +1,27 @@
+escala_sisco = {
+    'Nunca': 0,
+    'Casi Nunca': 1,
+    'Rara vez': 2,
+    'Algunas veces': 3,
+    'Casi siempre': 4,
+    'Siempre': 5,
+}
+
+mapa_semestre = {
+    '1 Semestre':0,'2 Semestre':1,'3 Semestre':2,'4 Semestre':3,
+    '5 Semestre':4,'6 Semestre':5,'7 Semestre':6,'8 Semestre':7,
+    '9 Semestre':8,'10 Semestre':9
+}
+
+mapa_asignaturas = {'1 a 3':0,'4 a 6':1,'7 a 9':2,'Mayor o igual a 10':3}
+
+mapa_promedio = {
+    'Menor a 3.0':0,'3.0 a 3.4':1,'3.5 a 3.9':2,'Mayor o igual a 4':3
+}
+
+mapa_horas = {
+    'Menos de 1 hora':0,'1-2 horas':1,'2-3 horas':2,
+    '3-5 horas':3,'6 o más horas':4
+}
+
+mapa_sino = {'Si':1,'No':0}
